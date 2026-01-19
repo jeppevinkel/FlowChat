6c49f6ad7e18a0a576bbb8b99e67e97fccc6eed4
Jeppe Beier <jeppevinkel@gmail.com>
2026-01-19 17:32:40 +0100

    Add more dice

--- FlowChat/Modules/RollModule.cs
+++ FlowChat/Modules/RollModule.cs
@@ -9,10 +9,15 @@ public class RollModule : InteractionModuleBase<SocketInteractionContext>
 
     public enum DiceType
     {
+        D2 = 2,
+        D3 = 3,
         D4 = 4,
         D6 = 6,
         D8 = 8,
-        D20 = 20
+        D10 = 10,
+        D12 = 12,
+        D20 = 20,
+        D100 = 100
     }
 
     [SlashCommand("roll", "Rolls a dnd dice")]
