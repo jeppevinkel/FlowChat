29fff095ad4adca65c7df7b0c9e49aab99305fff
Jeppe Beier <jeppevinkel@gmail.com>
2026-01-19 17:15:09 +0100

    Remove commented out code from entry function

--- FlowChat/Program.cs
+++ FlowChat/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Discord.WebSocket;
 using FlowChat.Services.Implementations;
-using FlowChat.Services.Interfaces;
-using HostInitActions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,21 +17,9 @@ class Program
         
         // Register the manager as singleton
         builder.Services.AddSingleton<GuildContextManager>();
-
-        // builder.Services.AddSingleton<VoiceChannelContext>();
-        // builder.Services.AddHostedService<DiscordService>();
         
         // Register services that can be used per-guild
         builder.Services.AddTransient<ElevenLabsService>();
-        
-        // builder.Services.AddSingleton<IBackgroundTaskQueue>(ctx =>
-        // {
-        //     if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
-        //         queueCapacity = 100;
-        //     return new BackgroundTaskQueue(queueCapacity);
-        // });
-        
-        // builder.Services.AddHostedService<VoiceChannelBackgroundService>();
 
         builder.Services.AddHostedService<DiscordService>();
         
